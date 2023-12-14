5da58d77455763591d5ee8da3dc449f88c8afa2e
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-15 02:43:34 +0600

    Implement renderworkoutmarker method.

--- script.js
+++ script.js
@@ -67,6 +67,8 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App {
   #map;
   #mapE;
+  #workout=[];
+
   constructor() {
     this._getPosition();
 
@@ -131,6 +133,8 @@ class App {
     const type= inputType.value;
     const distance= +inputDistance.value;
     const duration= +inputDuration.value;
+    const { lat, lng } = this.#mapE.latlng;
+    let workout;
     
 
     // If workout running , create running object
@@ -143,6 +147,9 @@ class App {
         !validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)
         )
         return alert('Input have to be positive numbers!')
+
+      workout= new Running([lat, lng], distance, duration, cadence);
+        
     }
     // If workout cycling , create cycling object
     if(type==='cycling'){
@@ -150,23 +157,17 @@ class App {
 
       if(!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) 
       return alert('Input have to be positive numbers!')
+
+    workout= new Cycling([lat, lng], distance, duration, elevation);
+    
     }
     // add new object to workout array
-    // render workout on map as marker
-    const { lat, lng } = this.#mapE.latlng;
-      L.marker([lat, lng])
-        .addTo(this.#map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('workout')
-        .openPopup();
+    this.#workout.push(workout);
+    console.log(workout)
+
+    // render workout on map as marker 
+    this.renderWorkoutMarker(workout);
+   
     // render workout on list 
 
 
@@ -178,6 +179,22 @@ class App {
         inputElevation.value =
           '';
       
+  }
+  renderWorkoutMarker(workout){
+
+     L.marker([lat, lng])
+     .addTo(this.#map)
+     .bindPopup(
+       L.popup({
+         maxWidth: 250,
+         minWidth: 100,
+         autoClose: false,
+         closeOnClick: false,
+         className: `${type}-popup`,
+       })
+     )
+     .setPopupContent(workout.distance)
+     .openPopup();
   }
 }
 
